2755d4ad478e2ff725f36b116f4d6c714950e328
some-du6e <103129948+some-du6e@users.noreply.github.com>
2025-03-18 19:18:27 -0600

    qdwq

---
+++ www/index.js
@@ -0,0 +1,11 @@
+// Bookmarklet functionality
+(function() {
+  // This function will be executed when the bookmarklet is clicked
+  function initLocalStorageManager() {
+    console.log("Localstorage Manager initialized");
+    // Add your localStorage management code here
+  }
+  
+  // Initialize the bookmarklet
+  initLocalStorageManager();
+})();
